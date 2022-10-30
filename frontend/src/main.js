a2d4453819de85eeddf87d949ae868a5e33a8f2a
Crispin <crispinliu168@gmail.com>
2022-10-30 13:20:06 -0700

    Remove pokemon db folder to just frontend

---
+++ frontend/src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import 'bootstrap-vue';
+import 'bootstrap';
+import SimpleTypeahead from 'vue3-simple-typeahead';
+
+
+import App from "./App.vue";
+import router from "./router";
+
+import "./assets/main.css";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(SimpleTypeahead);
+app.mount("#app");
---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,32 @@
+<script>
+</script>
+
+<template>
+  <div class="about flex-column justify-content-center">
+    <h1 class="d-flex justify-content-center">Search for a Pokemon</h1>
+    <div class="d-flex justify-content-center">
+    <vue3-simple-typeahead
+        id="typeahead_id"
+        placeholder="Start writing..."
+        :items="['One','Two','Three']"
+        :minInputLength="1"
+        :itemProjection="itemProjectionFunction"
+        @selectItem="selectItemEventHandler"
+        @onInput="onInputEventHandler"
+        @onFocus="onFocusEventHandler"
+        @onBlur="onBlurEventHandler"
+    >
+    </vue3-simple-typeahead>
+      </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
