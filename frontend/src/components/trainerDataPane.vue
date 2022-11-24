943aa50f4d34162f842c693aad888908f1409c29
szhou13 <szhou13@student.ubc.ca>
2022-11-24 13:54:23 -0800

    finsihed delete pokemon

--- frontend/src/components/trainerDataPane.vue
+++ frontend/src/components/trainerDataPane.vue
@@ -4,7 +4,7 @@
       <h2 class="name-title">
         {{ this.name }}
       </h2>
-      <table class="table table-bordered table-striped table-dark">
+      <table class="table table-bordered table-striped table-dark" v-if="renderComponent">
         <thead>
         <tr>
           <th scope="col">Name</th>
@@ -24,10 +24,12 @@
           <td><img :src=pokemon.sprite alt="pokemon_sprite"/></td>
           <td>
             <div class="d-flex justify-content-evenly">
-              <button class="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#editModal" v-on:click="editForm(pokemon.trained_pokemon_id)">
+              <button class="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#editModal"
+                      v-on:click="editForm(pokemon.trained_pokemon_id)">
                 <i class="bi bi-pencil-fill"> Edit</i>
               </button>
-              <button class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#deleteModal" v-on:click="editForm(pokemon.trained_pokemon_id)">
+              <button class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#deleteModal"
+                      v-on:click="deleteForm(pokemon.trained_pokemon_id, pokemon.trainer_id)">
                 <i class="bi bi-trash"> Delete</i>
               </button>
             </div>
@@ -37,7 +39,7 @@
       </table>
     </div>
   </div>
-<!-- Edit Modal Pop-up -->
+  <!-- Edit Modal Pop-up -->
   <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
@@ -67,7 +69,7 @@
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="submit" class="btn btn-primary" v-on:click="updatePokemon()" data-bs-dismiss="modal">Save Changes</button>
+          <button type="submit" class="btn btn-primary" v-on:click="updatePokemon()" data-bs-dismiss="modal">Save Changes </button>
         </div>
       </div>
     </div>
@@ -82,11 +84,14 @@
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          Are you sure you want to delete {{this.pokemon.name}}?
+          <input type="hidden" value="" id="delete-pokemon-id"/>
+          <input type="hidden" value="" id="trainer-id"/>
+          Are you sure you want to delete {{ this.pokemon.name }}?
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Delete</button>
+          <button type="button" class="btn btn-primary" v-on:click="deletePokemon()" data-bs-dismiss="modal">Delete
+          </button>
         </div>
       </div>
     </div>
@@ -98,6 +103,7 @@
 import {gsap} from "gsap";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {nextTick} from "vue";
 
 export default {
   props: ["trainerData"],
@@ -106,13 +112,51 @@ export default {
       name: null,
       gender: null,
       pokemon: [],
+      trainer_id: null,
+      renderComponent: true,
     };
   },
   components: {},
   methods: {
 
+    async forceRerender() {
+      this.renderComponent = false;
+      await nextTick();
+      this.renderComponent = true;
+      console.log("rerendered...")
+      console.log(this.$data.pokemon)
+    },
+
     updatePokemon() {
       console.log("THIS HSOULD WORK BUT FOR NOW")
+    },
+
+    deletePokemon() {
+      console.log('enter delete')
+      const delete_pokemon_id = document.getElementById('delete-pokemon-id').value;
+      const trainer_id = document.getElementById('trainer-id').value;
+      fetch("http://127.0.0.1:5000/trainer/" + trainer_id + "/pokemon/" + delete_pokemon_id, {
+        method: "DELETE"
+      }).then(() => {
+        let index = -1;
+        for (let i = 0; i < this.pokemon; i++) {
+          if (this.pokemon[i].pokemon_trained_id === Number(delete_pokemon_id)) {
+            index = i;
+            break;
+          }
+        }
+        console.log("INDEX: " + index)
+        this.$data.pokemon.splice(index,1)
+        console.log('BEFORE FORCE RENDER')
+        console.log(this.$data.pokemon)
+        this.forceRerender();
+      })
+    },
+    deleteForm(trained_pokmeon_id, trainer_id) {
+      const delete_id_field = document.getElementById('delete-pokemon-id')
+      const trainer_id_field = document.getElementById('trainer-id')
+      delete_id_field.value = trained_pokmeon_id;
+      trainer_id_field.value = trainer_id;
     },
     editForm(trained_pokemon_id) {
       console.log(trained_pokemon_id);
@@ -140,13 +184,16 @@ export default {
     },
   },
   mounted() {
-    console.log(this.$props.trainerData)
+    console.log("mounted")
     let pane = this.$refs.pane;
     pane.style.opacity = 1;
 
     this.name = this.$props.trainerData.name;
     this.gender = this.$props.trainerData.gender;
     this.pokemon = this.$props.trainerData.pokemon;
+    console.log(this.pokemon)
+    this.trainer_id = this.$props.trainerData.trainer_id;
+
     console.log(this.pokemon)
     this.toggleData(true);
     gsap.fromTo(
@@ -262,13 +309,13 @@ export default {
   text-fill-color: transparent;
   padding-top: 4vh;
   padding-bottom: 4vh;
-  background-clip:content-box;
+  background-clip: content-box;
   box-shadow: #212529;
   background-color: #212529;
 }
 
 .pane-background {
-  background-clip:content-box;
+  background-clip: content-box;
   box-shadow: #212529;
   background-color: #212529;
 }
